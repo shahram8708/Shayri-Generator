5c44d3a36c003e516128d1090ab83a4d0002e129
Shah Ram <152763547+shahram8708@users.noreply.github.com>
2024-04-27 17:55:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template, request
 import requests
 import logging
 
-app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
-
 logging.basicConfig(level=logging.DEBUG)
 
-API_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyBfaJZk-ljrgGrnmOeg4wrQ3raKdhQnF1c"
+API_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyBt_UKi-gC5PM9VsIZ0TjoJMAVKsYM0KVU"
 
 DEFAULT_TEXT = "Write me poetry. But poetry should be in Hindi. But it should be written in English."
 
@@ -22,10 +20,14 @@ def search():
         response = generate_story(DEFAULT_TEXT + search_query)  
         logging.debug(f"API response: {response}")
         if response and 'candidates' in response:
-            story_content = extract_story_content(response)
+            story_content, safety_ratings = extract_story_and_safety(response)
             if story_content:
-                story_content = story_content.replace('*', "")  
-                return render_template('result.html', story_content=story_content)
+                if is_safe_content(safety_ratings):
+                    story_content = story_content.replace('*', "")  
+                    return render_template('result.html', story_content=story_content)
+                else:
+                    error_msg = "Failed to generate Shayri content due to safety concerns. Please try again with a different query."
+                    return render_template('index.html', error=error_msg)
         error_msg = "Failed to generate Shayri content. Please try again."
         return render_template('index.html', error=error_msg)
     else:
@@ -43,15 +45,24 @@ def generate_story(prompt):
     else:
         return None
 
-def extract_story_content(response):
-    candidates = response['candidates']
+def extract_story_and_safety(response):
+    candidates = response.get('candidates', [])
     if candidates:
-        content = candidates[0]['content']
+        candidate = candidates[0]
+        content = candidate.get('content', {}).get('parts', [])
         if content:
-            parts = content['parts']
-            if parts:
-                return parts[0].get('text', '')
-    return None
+            story_content = content[0].get('text', '')
+        else:
+            story_content = None
+        safety_ratings = candidate.get('safetyRatings', [])
+        return story_content, safety_ratings
+    return None, None
+
+def is_safe_content(safety_ratings):
+    for rating in safety_ratings:
+        if rating['probability'] == 'HIGH':
+            return False
+    return True
 
 if __name__ == '__main__':
     app.run(debug=True)
